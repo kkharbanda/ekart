4fdae0a292e6a30f7d27a735f2142fd03baef34a
komalkharbanda <kkharbanda76@gmail.com>
2023-03-24 10:18:57 +0530

    changes

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ import cors from "cors";
 
 const app = express();
 export default app;
-dotenv.config({
-  path: "./config/config.env",
-});
+// dotenv.config({
+//   path: "./config/config.env",
+// });
  dotenv.config()
 
 // Using Middlewares
